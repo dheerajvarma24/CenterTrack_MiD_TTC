056bf41b34d5890bf84cb3ff56dbaeef887ae873
dheeraj <macharav@rhrk.uni-kl.de>
2023-05-08 16:37:14 +0200

    added proper comments for the previous commit changes

--- src/lib/detector.py
+++ src/lib/detector.py
@@ -32,8 +32,6 @@ class Detector(object):
     self.model = create_model(
       opt.arch, opt.heads, opt.head_conv, opt=opt)
     self.model = load_model(self.model, opt.load_model, opt)
-    print(opt.device)
-    print('--------------')
     self.model = self.model.to(opt.device)
     self.model.eval()
 
@@ -138,7 +136,7 @@ class Detector(object):
 
     # merge multi-scale testing results
     results = self.merge_outputs(detections)
-    # torch.cuda.synchronize()
+    # torch.cuda.synchronize() # enable for gpu and comment out for cpu
     end_time = time.time()
     merge_time += end_time - post_process_time
     
@@ -337,17 +335,17 @@ class Detector(object):
   def process(self, images, pre_images=None, pre_hms=None,
     pre_inds=None, return_time=False):
     with torch.no_grad():
-      # torch.cuda.synchronize()
+      # torch.cuda.synchronize() # enable for gpu and comment out for cpu
       output = self.model(images, pre_images, pre_hms)[-1]
       output = self._sigmoid_output(output)
       output.update({'pre_inds': pre_inds})
       if self.opt.flip_test:
         output = self._flip_output(output)
-      # torch.cuda.synchronize()
+      # torch.cuda.synchronize() # enable for gpu and comment out for cpu
       forward_time = time.time()
       
       dets = generic_decode(output, K=self.opt.K, opt=self.opt)
-      # torch.cuda.synchronize()
+      # torch.cuda.synchronize() # enable for gpu and comment out for cpu
       for k in dets:
         dets[k] = dets[k].detach().cpu().numpy()
     if return_time:
