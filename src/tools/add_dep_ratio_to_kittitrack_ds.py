3c348688e8d8b5e83b46acfc76c22c850be13908
dheeraj <macharav@rhrk.uni-kl.de>
2023-09-21 22:43:33 +0200

    added MiD helper files

---
+++ src/tools/add_dep_ratio_to_kittitrack_ds.py
@@ -0,0 +1,98 @@
+# Warning: It is an inplace edit file. So, make sure to take a backup of the original file before running this script.
+import numpy as np
+import fileinput
+
+
+class GTDepRatio(object):
+    def __init__(self, gt_path):
+        self.index = 0
+        self.gt_path = gt_path
+        self.input_dict = {}
+        self.output_dict = {}
+
+    def readinput(self):
+        with open(self.gt_path, 'r') as f:
+            for line in f.readlines():
+                line = line.strip('\n')
+                row = line.split(' ')
+                frameid = row[0]
+                trackid = row[1]
+                depth = row[-2] # row -2 is the depth info in gt
+                if frameid in self.input_dict:
+                    self.input_dict[frameid].append((trackid, depth))
+                else:
+                    self.input_dict[frameid] = [(trackid, depth)]
+        return self.input_dict
+
+    def add_depth_ratio(self):
+        self.index = 0
+        # Loop through all the the frames in gt
+        for frameid in self.input_dict.keys():
+            # Objects in the first frame have no depth ratio. 
+            if frameid == str(0):
+                for trackid, depth in self.input_dict[frameid]:
+                    key = frameid + '_' + trackid
+                    self.output_dict[key] = np.nan
+                continue
+
+            # Loop through all the objects in the current frame.
+            for trackid, depth in self.input_dict[frameid]:
+                
+                # Ignore the Dontcare objects in the gt
+                if trackid == str(-1):
+                    key = frameid + '_' + trackid
+                    self.output_dict[key] = np.nan
+                    continue
+                
+                # First assigning all the objects with nan, later we will replace the nan with actual depth ratio.
+                key = frameid + '_' + trackid
+                self.output_dict[key] = np.nan
+
+                # Few frame are missing for example:240 frame in file 5; 318 frame in file 9
+                if str(int(frameid)-1) not in self.input_dict.keys():
+                    continue
+
+                # Loop through all the objects in the previous frame.
+                for trackid_prev, depth_prev in self.input_dict[str(int(frameid)-1)]:
+                    # If the object is present in the previous frame, then calculate the depth ratio.
+                    if trackid == trackid_prev:
+                        depth_ratio = round(float(depth) / float(depth_prev), 6)
+                        key = frameid + '_' + trackid
+                        self.output_dict[key] = str(depth_ratio)
+                        continue
+        
+        return self.output_dict
+
+
+    # This function is used to edit the groundtruth files inplace.
+    def inplace_edit(self):
+        # Open the file for in-place editing
+        with fileinput.FileInput(self.gt_path, inplace=True) as file:
+            # Iterate over each line in the file
+            for line in file:
+                # Modify the line as desired
+                new_line = line.strip()
+                row = new_line.split(' ')
+                key = row[0] + '_' + row[1]
+                if key in self.output_dict.keys():
+                    # Append the depth ratio to the end of the line.
+                    row.append(self.output_dict[key])
+                    new_line = ' '.join([str(i) for i in row]) + '\n'
+                else:
+                    new_line = line + '\n'
+                # Write the modified line back to the file.
+                # NOTE: Here the print statement is explicitly writing the content to the file.
+                print(new_line, end='')
+
+
+if __name__ == '__main__':
+    for i in range (0, 21):
+        if i < 10:
+            filename = '../../data/kitti_tracking/label_02/000'+str(i)+'.txt'
+        else:
+            filename = '../../data/kitti_tracking/label_02/00'+str(i)+'.txt'
+        gtDepRatio = GTDepRatio(filename)
+        gtDepRatio.readinput()
+        gtDepRatio.add_depth_ratio()
+        gtDepRatio.inplace_edit()
+        print('processed file: ', filename)
---
+++ src/tools/cal_mid_loss.py
@@ -0,0 +1,153 @@
+# Script to calculate motion in depth loss.
+import math
+import argparse
+import time
+
+class CalMIDLoss:
+    def __init__(self, gt_path, pred_path):
+        self.gt_path = gt_path
+        self.pred_path = pred_path
+        self.gt_data = {}
+        self.pred_data = {}
+        self.matched_data = {}
+        self.losses = []
+
+    # While reading the data from the gt and pred files, store the data in a dictionary with key as 'frameid_trackid' and value as a list of [(l,t,r,b), dep_ratio, type]
+    def read_data_from_files(self, path, filetype):
+        with open(path, 'r') as f:
+            for line in f.readlines():
+                line = line.strip('\n')
+                row = line.split(' ')
+                frameid = row[0]
+                trackid = row[1]
+                classid = row[2]
+                l = float(row[6])
+                t = float(row[7])
+                r = float(row[8])
+                b = float(row[9])
+                key = frameid + '_' + trackid
+
+                if filetype == 'gt':
+                    confidence_score = 1
+                    dep_ratio = row[-1] # there are nan values in gt, so let it be string not float(row[17])
+                    self.gt_data[key] = [(l,t,r,b), dep_ratio, confidence_score, classid]
+                elif filetype == 'pred':
+                    confidence_score = float(row[-2])
+                    dep_ratio = float(row[-1])
+                    self.pred_data[key] = [(l,t,r,b), dep_ratio, confidence_score, classid]
+
+
+
+    # Match gt data with pred data based on frameid and trackid.
+    def match_pred_with_gt(self):
+        for gt_key in self.gt_data.keys():
+            gt_frameid = gt_key.split('_')[0] # gt_key consists of 'frameid_trackid'
+            current_score = 0
+
+            for pred_key in self.pred_data.keys():
+                # get all the pred values in the same frame as gt.
+                if pred_key.startswith(gt_frameid) and self.pred_data[pred_key][-1].lower() == self.gt_data[gt_key][-1].lower(): # check for class id also 
+                    
+                    # calculate IOU between the two bounding boxes.
+                    pred_box = self.pred_data[pred_key][0]
+                    gt_box = self.gt_data[gt_key][0]
+                    iou_score = CalMIDLoss.calculate_iou(pred_box, gt_box)
+
+                    if iou_score < 0.5:
+                        continue # ignore the object with iou < 0.5
+
+                    # Store the matched data with the maximum iou score.
+                    if iou_score > current_score:
+                        self.matched_data[gt_key] = [self.gt_data[gt_key], self.pred_data[pred_key]]
+                        current_score = iou_score
+                    
+                    '''elif iou_score == current_score:
+                        # if IOU score is same, then check for the maximum detection condidence score for the object.
+                        if self.pred_data[pred_key][2] > self.matched_data[gt_key][1][2]:
+                            self.matched_data[gt_key] = [self.gt_data[gt_key], self.pred_data[pred_key]]
+                            current_score = iou_score'''
+    
+
+    def calculate_iou(pred_box, gt_box):
+        # Calculate the intersection area
+        # inter_area = (min(pred_box[2], gt_box[2]) - max(pred_box[0], gt_box[0])) * (min(pred_box[3], gt_box[3]) - max(pred_box[1], gt_box[1]))
+        x1, y1, x2, y2 = pred_box
+        x3, y3, x4, y4 = gt_box
+        
+        xi1 = max(x1, x3)
+        yi1 = max(y1, y3)
+        xi2 = min(x2, x4)
+        yi2 = min(y2, y4)
+        inter_area = max(xi2 - xi1, 0) * max(yi2 - yi1, 0)
+
+        # Calculate the union area
+        pred_area = (pred_box[2] - pred_box[0]) * (pred_box[3] - pred_box[1])
+        gt_area = (gt_box[2] - gt_box[0]) * (gt_box[3] - gt_box[1])
+        union_area = pred_area + gt_area - inter_area
+
+        iou_score = inter_area / union_area
+        return iou_score
+
+    # Read the dep_ratio info from the matched_data obtained from above and calculate the loss.
+    def cal_MID_Loss(self):
+
+        # Loop through all the matched data
+        for matched_key in self.matched_data.keys():
+            gt_dep_ratio = self.matched_data[matched_key][0][1]
+            pred_dep_ratio = self.matched_data[matched_key][1][1]
+            
+            # Ignore the nan values in gt and pred
+            if str(gt_dep_ratio).lower() == 'nan' or str(pred_dep_ratio).lower() == 'nan':
+                continue            
+            # (optional) Ignore the objects with dep ratio > 1
+            if float(gt_dep_ratio) > 1 : # or float(pred_dep_ratio) > 1:
+                continue
+
+            loss = abs(math.log(pred_dep_ratio) - math.log(float(gt_dep_ratio)))
+            self.losses.append(loss)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Enter gt and pred validation file paths')
+
+    # Add the required arguments
+    parser.add_argument('--gt_path', type=str, help='Enter gt val file path')
+    parser.add_argument('--pred_path', type=str, help='Enter pred val file path')
+
+    # Parse the arguments
+    args = parser.parse_args()
+
+    # Check if the arguments are provided
+    if not args.gt_path or not args.pred_path:
+        print('provide the gt and pred validation file paths as arguments')
+    
+    # Read the gt and pred validation file paths
+    gt_val_file_path = args.gt_path
+    pred_val_file_path = args.pred_path
+
+    # hard coded path
+    #gt_val_file_path = './eval_kitti_track/data/tracking/label_02_val_MiD/'
+    #pred_val_file_path = '../../exp/tracking,ddd/kitti_half_MiD_test_EXP7/results_kitti_tracking/'
+
+    final_loss = {}
+    # 4 and 11 are validatoin data files.
+    t0 = time.time()
+    for i in [4,11]:
+        if i < 10:
+            gt_path = gt_val_file_path + '/000' + str(i) + '.txt'
+            pred_path = pred_val_file_path + '/000' + str(i) + '.txt'
+        else:
+            gt_path = gt_val_file_path + '/00' + str(i) + '.txt'
+            pred_path = pred_val_file_path + '/00' + str(i) + '.txt'
+
+        calMIDLoss = CalMIDLoss(gt_path, pred_path)
+        calMIDLoss.read_data_from_files(gt_path, 'gt')
+        calMIDLoss.read_data_from_files(pred_path, 'pred')
+        calMIDLoss.match_pred_with_gt()
+        calMIDLoss.cal_MID_Loss()
+        final_loss[i] = sum(calMIDLoss.losses)/len(calMIDLoss.losses)  * 10000
+        print(f'file name {pred_path}, loss {final_loss[i]:.4f}')
+        print(f'file name {pred_path}, len of the file {len(calMIDLoss.losses)}')
+    print("###### time for MiD Loss calculation on val set [4,11] #########  ", time.time() - t0)
+    print(f'Average MiD Loss {sum(list(final_loss.values()))/len(list(final_loss.values())):.4f}')
+    
